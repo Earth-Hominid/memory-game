b29788bf9ba13468f99fe04395f84acdcff8c0fa
C <earthhominid@gmail.com>
2021-12-09 17:05:44 -0300

    update stylesheet

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -1,14 +1,53 @@
-import marvel from './images/marvel.png';
+import { useState } from 'react';
+import { useEffect } from 'react/cjs/react.development';
+import quotes from './utils/quotes';
+
+const quotesArray = quotes;
 
 const Scoreboard = () => {
+  const [startingQuote, setStartingQuote] = useState('');
+
+  const getRandomQuote = () => {
+    const randomStartingQuote = '';
+
+    for (let i = 0; i < quotesArray.length; i++) {
+      const randomStartingQuote =
+        quotesArray[Math.floor(Math.random() * quotesArray.length)];
+      return randomStartingQuote;
+    }
+    setStartingQuote(randomStartingQuote);
+  };
+
+  // Use effect used to get random quote and dynamically update if it changes.
+
+  useEffect(() => {
+    getRandomQuote();
+  }, [startingQuote]);
+
   return (
     <>
       <div className="board_area">
         <div className="score_board">
-          <div className="score_container">
-            <h3>Score:</h3>
-            <h3>Level:</h3>
-            <h3>Highscore:</h3>
+          <div className="quote_container">
+            <h2 className="quote">{startingQuote}</h2>
+            <div className="score_wrapper">
+              <div className="score_container">
+                <h3 className="score_heading">SCORE</h3>
+                <h3 className="score">1</h3>
+              </div>
+              <div className="score_container">
+                <h3 className="score_heading">LEVEL</h3>
+                <h3 className="score">1</h3>
+              </div>
+              <div className="score_container">
+                <h3 className="score_heading">HIGHEST SCORE</h3>
+                <h3 className="score">1</h3>
+              </div>
+              <div className="score_container">
+                <h3 className="score_heading">HIGHEST LEVEL</h3>
+                <h3 className="score">1</h3>
+              </div>
+            </div>
           </div>
         </div>
       </div>
