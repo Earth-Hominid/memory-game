248a1a50ce6a5fe65ac136b5380f3243087fe5df
C <earthhominid@gmail.com>
2021-12-30 12:32:18 -0300

    removed shuffle node test files

--- src/test1.js
+++
@@ -1,7 +0,0 @@
-const prevCards = [{ A: 1 }, { B: 2 }, { C: 3 }, { D: 4 }];
-
-const shuffle = (array) => {
-  return [...array].sort(() => Math.random() - 0.5);
-};
-
-console.log(shuffle(prevCards));
